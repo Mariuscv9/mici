8d4bf1c8fc040ddd9402304864b06a51fce527ef
Mariuscv9 <enachemarius2012@gmail.com>
2023-07-29 20:40:12 +0300

    Revert "v2"
    This reverts commit 50d05327d79386a20a3a82b0ebe6d6d6a063e9ad.

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,11 +1,5 @@
 import React from "react";
 
 export default function Forecast(props) {
-  console.log(props.dates);
-
-  return (
-    <div className="daywrap">
-      <p>{props.dates}</p>
-    </div>
-  );
+  return <div>forecast</div>;
 }
