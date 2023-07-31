33c441e7ab1f58a24869b7164023132d497120b0
Mariuscv9 <enachemarius2012@gmail.com>
2023-07-31 18:13:48 +0300

    trimmed

--- app/layout.js
+++ app/layout.js
@@ -17,8 +17,8 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header />
         {children}
-        <Footer />
       </body>
+        <Footer />
     </html>
   );
 }
