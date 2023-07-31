38e1dbb057d96e94435cc15bb7a428e67f596172
Mariuscv9 <enachemarius2012@gmail.com>
2023-07-31 21:41:05 +0300

    deploy

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,6 +1,6 @@
 "use client";
 
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 import Modalcomp from "components/Modal";
 import Image from 'next/image'
 
@@ -8,8 +8,8 @@ export default function Forecast(props) {
   console.log(props.data);
   console.log(props.curent);
   const toggleStyle2 = (props.curent.current.is_day = 1
-    ? "day col-12 col-md-3 mt-5 location_day"
-    : "day col-12 col-md-3 location_night");
+    ? "day location_day"
+    : "day location_night");
   let percentage = props.data.day.daily_chance_of_rain;
   let reversedPercentage = 100 - percentage;
   return (
